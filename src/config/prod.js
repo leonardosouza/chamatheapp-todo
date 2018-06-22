bfd2d9a001ce4f4b404b760ad6714eae3447af4f
Leonardo Souza <leonardodisouza@gmail.com>
2018-06-22 16:44:46 -0300

    fixing keys

---
+++ src/config/prod.js
@@ -0,0 +1,10 @@
+let FirebaseConfig = {
+  apiKey: "AIzaSyDDYlsQaG6IRYVrewAhU_auSSl2qyPePDk",
+  authDomain: "todochama-129ea.firebaseapp.com",
+  databaseURL: "https://todochama-129ea.firebaseio.com",
+  projectId: "todochama-129ea",
+  storageBucket: "todochama-129ea.appspot.com",
+  messagingSenderId: "356212282803"
+};
+
+module.exports = { FirebaseConfig };
